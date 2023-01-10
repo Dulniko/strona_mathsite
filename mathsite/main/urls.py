1df56ab216ce0648523c2ab4a517717768b50b19
Dulniko <jakub.dulnik03@gmail.com>
2023-01-10 16:52:56 +0100

    ranking50

--- mathsite/main/urls.py
+++ mathsite/main/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("ranking/", views.ranking, name="ranking"),
+    path("", views.mainpage, name="mainpage"),
+    path("rankingi/", views.rankingi, name="rankingi"),
+    path("ranking10/", views.ranking10, name="ranking10"),
+    path("ranking50/", views.ranking50, name="ranking50"),
     path("mnozenie/", views.mnozenie, name="mnozenie"),
     path("mnozenie10/", views.mnozenie10, name="mnozenie10"),
+    path("mnozenie50/", views.mnozenie50, name="mnozenie50"),
 ]
--- mathsite/main/views.py
+++ mathsite/main/views.py
@@ -5,20 +5,35 @@ from .models import RankingTo10, RankingTo50
 from random import randint
 from ast import literal_eval
 
-def index(response):
+def mainpage(response):
     return render(response, "main/mainpage.html")
 
-def ranking(request):
+def rankingi(request):
+    return render(request, "main/rankingi.html")
+
+def ranking10(request):
     sort_by = request.POST.get("sort_by", "-Score")
-    rk10 = RankingTo10.objects.all().order_by(sort_by)
+    rk = RankingTo10.objects.all().order_by(sort_by)
     
     filter_by = request.POST.get('filter_by', '')
 
     if filter_by:
-        rk10 = RankingTo10.objects.filter(PlayerName=request.user.username).order_by(sort_by)
+        rk = RankingTo10.objects.filter(PlayerName=request.user.username).order_by(sort_by)
     else:
-        rk10 = RankingTo10.objects.all().order_by(sort_by)
-    return render(request, "main/ranking.html", {"rk10":rk10})
+        rk = RankingTo10.objects.all().order_by(sort_by)
+    return render(request, "main/ranking.html", {"rk":rk})
+
+def ranking50(request):
+    sort_by = request.POST.get("sort_by", "-Score")
+    rk = RankingTo50.objects.all().order_by(sort_by)
+    
+    filter_by = request.POST.get('filter_by', '')
+
+    if filter_by:
+        rk = RankingTo50.objects.filter(PlayerName=request.user.username).order_by(sort_by)
+    else:
+        rk = RankingTo50.objects.all().order_by(sort_by)
+    return render(request, "main/ranking.html", {"rk":rk})
 
 def mnozenie(request):
     return render(request, "main/mnozenie.html")
@@ -66,5 +81,49 @@ def mnozenie10(request):
         first_question = [randint(1, 10), randint(1, 10)]
         return render(request, 'main/mnozenie10.html', {'question': first_question, 'correct_answers': request.session['correct_answers'], 'nr_question': request.session['nr']})
     
+def mnozenie50(request):
+    if (request.method == 'POST') and (request.session.get('nr', 0) < 50):
+        request.session['nr'] = request.session.get('nr', 0) + 1
+        # print(request.POST['current_answer'])
+        # print(request.POST['current_question'])
+        # pobieramy aktualne pytanie i odpowiedź z formularza
+        current_question = literal_eval(request.POST['current_question'])
+        if request.POST['current_answer']:
+            current_answer = int(request.POST['current_answer'])
+        else:
+            current_answer = "101"
+        #sprawdzanie czy takie same 
+        is_answer_correct = (current_question[0]*current_question[1]) == current_answer
+        if is_answer_correct:
+            request.session['correct_answers'] = request.session.get('correct_answers', 0) + 1
+        
+        # pobieramy kolejne pytanie
+        next_question = [randint(1, 10), randint(1, 10)]
+        
+        return render(request, 'main/mnozenie10.html', {'question': next_question, 'correct_answers': request.session['correct_answers'], 'nr_question': request.session['nr']})
+    elif request.session.get('nr', 0) == 50:
+        #dodanie do bazy
+        if request.user.is_authenticated:
+            m = RankingTo50(PlayerName = request.user.username, Score = request.session['correct_answers'])
+            m.save()
+        else:
+            m = RankingTo50(PlayerName = "Anonim", Score = request.session['correct_answers'])
+            m.save()
+        number_of_q = request.session['nr']
+        number_of_correct_a = request.session['correct_answers']
+        if (request.session['correct_answers']*2)>=request.session['nr']:
+            gz = True
+        else:
+            gz = False
+        request.session['nr'] = 0
+        request.session['correct_answers'] = 0
+        return render(request, "main/congratulations.html", {'correct_answers': number_of_correct_a, 'nr_question': number_of_q, "gz":gz})
+    else:
+        request.session['nr'] = 0
+        request.session['correct_answers'] = 0
+        first_question = [randint(1, 10), randint(1, 10)]
+        return render(request, 'main/mnozenie10.html', {'question': first_question, 'correct_answers': request.session['correct_answers'], 'nr_question': request.session['nr']})
+    
+
 
 
